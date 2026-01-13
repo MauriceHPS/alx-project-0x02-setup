a55fe83fe6d1e3cf04317cf00b5ac843c96b134b
MauriceHPS <maurice@headpoint.info>
2026-01-13 10:46:54 +0300

    Implemented Basic Routing in NextJS

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <div className="flex flex-col">
+        <ul>
+          <li>
+            <Link href="/home">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li></li>
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Header;
